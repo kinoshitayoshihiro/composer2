921315142fa21ca0ea11af7073eff2a92039ca14
Harugoro Shichimi <shimogami88@gmail.com>
2025-06-12 04:08:18 +0900

    Update onset heatmap implementation

--- build_all_json.py
+++ build_all_json.py
@@ -3,47 +3,8 @@
 
 import sys
 import json
-from music21 import converter, note, chord, meter
-
-
-def build_heatmap(midi_path, resolution):
-    # （先ほどの onset_heatmap.py と同じ内容。略。）
-    score = converter.parse(midi_path)
-    ts = score.recurse().getElementsByClass(meter.TimeSignature)
-    if len(ts) == 0:
-        raise RuntimeError("拍子情報が見つかりませんでした。")
-    first_ts = ts[0]
-    beats_per_measure = first_ts.numerator
-    beat_unit = first_ts.denominator
-    quarter_per_measure = beats_per_measure * (4.0 / beat_unit)
-
-    onset_offsets = []
-    for el in score.recurse():
-        if isinstance(el, note.Note) or isinstance(el, chord.Chord):
-            onset_offsets.append(el.offset)
-    if len(onset_offsets) == 0:
-        raise RuntimeError("ノート（Note/Chord）のオンセットが見つかりませんでした。")
-
-    max_offset = max(onset_offsets)
-    total_measures = int(max_offset // quarter_per_measure) + 1
-    total_grids = total_measures * resolution
-    heatmap_counts = [0] * total_grids
-
-    for off in onset_offsets:
-        measure_index = int(off // quarter_per_measure)
-        offset_in_measure = off - (measure_index * quarter_per_measure)
-        subbeat_index = int((offset_in_measure / quarter_per_measure) * resolution)
-        if subbeat_index >= resolution:
-            subbeat_index = resolution - 1
-        grid_index = measure_index * resolution + subbeat_index
-        if 0 <= grid_index < total_grids:
-            heatmap_counts[grid_index] += 1
-
-    heatmap_list = []
-    for idx, cnt in enumerate(heatmap_counts):
-        heatmap_list.append({"grid_index": idx, "count": cnt})
-
-    return heatmap_list
+from music21 import converter, meter
+from utilities.onset_heatmap import build_heatmap
 
 
 def extract_tempo_map(midi_path):

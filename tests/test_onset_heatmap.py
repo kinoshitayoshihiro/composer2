d8e3e9deb02ef7c2f764d298f8daba692e0fcd4c
Harugoro Shichimi <shimogami88@gmail.com>
2025-06-13 02:00:13 +0900

    tests: update rhythm library fixture and imports

--- tests/test_onset_heatmap.py
+++ tests/test_onset_heatmap.py
@@ -1,10 +1,5 @@
-import os
-import sys
 import tempfile
 import pretty_midi
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from utilities.onset_heatmap import build_heatmap
 
 

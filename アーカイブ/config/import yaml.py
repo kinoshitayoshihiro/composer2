3991f275aae2126210656721a2e8ca2182ea7699
Harugoro Shichimi <shimogami88@gmail.com>
2025-06-12 20:17:39 +0900

    Delete アーカイブ directory

--- アーカイブ/config/import yaml.py
+++
@@ -1,4 +0,0 @@
-import yaml
-with open("data/processed_chordmap_with_emotion.yaml", "r", encoding="utf-8") as f:
-    data = yaml.safe_load(f)
-print("Sections:", list(data.get("sections", {}).keys()))

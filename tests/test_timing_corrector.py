9de968e494cecc1d415affef3ffa53bf96be25ca
Harugoro Shichimi <shimogami88@gmail.com>
2025-07-02 08:24:52 +0900

    chore(deps): include setuptools in test requirements

--- tests/test_timing_corrector.py
+++ tests/test_timing_corrector.py
@@ -12,6 +12,7 @@ def test_timing_corrector_reduces_jitter() -> None:
         part.insert(o, n)
     tc = TimingCorrector(alpha=0.5)
     corrected = tc.correct_part(part)
+    assert corrected.notes[0].offset == offsets[0]
     orig_res = [o - round(o) for o in offsets]
     new_res = [n.offset - round(n.offset) for n in corrected.notes]
     assert statistics.pstdev(new_res) < statistics.pstdev(orig_res)

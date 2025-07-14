bb63783175e22035948683b3706df5ef4ac4b469
Harugoro Shichimi <shimogami88@gmail.com>
2025-07-15 01:18:13 +0900

    chore: remove outdated model fixture

---
+++ utilities/types.py
@@ -0,0 +1,7 @@
+"""Common type aliases used across utilities."""
+from typing import Literal
+
+Intensity = Literal["low", "mid", "high"]
+AuxTuple = tuple[str, str, str]
+
+__all__ = ["Intensity", "AuxTuple"]

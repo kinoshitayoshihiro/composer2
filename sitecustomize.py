267e6f0e2ba1078d2fcb59f6e74bbca3ce8816cd
Harugoro Shichimi <shimogami88@gmail.com>
2025-07-22 14:11:04 +0900

    Only stub optional modules if missing

--- sitecustomize.py
+++ sitecustomize.py
@@ -2,10 +2,12 @@
 Silences heavy optional dependencies in minimal CI environments.
 """
 import importlib.machinery
+import importlib.util
 import sys
 import types  # pragma: no cover
 
-for _name in ("pkg_resources", "yaml", "scipy", "scipy.signal"):
-    mod = types.ModuleType(_name)
-    mod.__spec__ = importlib.machinery.ModuleSpec(_name, loader=None)
-    sys.modules.setdefault(_name, mod)  # pragma: no cover
+for _name in ("pkg_resources", "yaml", "scipy", "scipy.signal", "music21"):
+    if importlib.util.find_spec(_name) is None:
+        mod = types.ModuleType(_name)
+        mod.__spec__ = importlib.machinery.ModuleSpec(_name, loader=None)
+        sys.modules.setdefault(_name, mod)  # pragma: no cover
